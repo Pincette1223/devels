f1b1a86199a4c3fc17c7eda72ee98447acdaae1b
Pincette1223 <vlstpt812@gmail.com>
2021-12-07 16:35:19 +0900

    그림자 추가

--- js/background.js
+++ js/background.js
@@ -1,16 +1,10 @@
-const images = [
-	'0.jpeg',
-	'1.jpeg',
-	'2.jpeg',
-	'3.jpeg',
-	'4.jpeg'
-];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement('img');
+const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-document.body.style.backgroundRepeat = 'no-repeat';
+document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0),rgba(0, 0, 0, 0)), url(${bgImage.src})`;
